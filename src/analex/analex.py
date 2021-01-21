d8fbed751fdc175e1c6380ab86cb149c32b6412d
Jorge Reyes <40706868+imreyesjorge@users.noreply.github.com>
2021-01-21 06:55:32 +0000

    feat: Create syntax.py

--- src/analex/analex.py
+++ src/analex/analex.py
@@ -1,17 +1,24 @@
 import sys
 sys.path.append('src/')
 
-from syntax import syn
+from syntax import *
 
 # Check correct usage
 if(len(sys.argv) == 2):
 
   # Check if it is a '.mio' file
   if(sys.argv[1].split('.')[1] == 'mio'):
+    file = open(sys.argv[1])
     fileLex = open('{}.lex'.format(sys.argv[1].split('.')[0]), 'w+')
     fileSim = open('{}.sim'.format(sys.argv[1].split('.')[0]), 'w+')
 
-    print(syn['RES'])
+    for line in file:
+      if(line[0] != "#"):
+        if(isLine(line.split())):
+          print(line, end = '')
+        else:
+          print('ERROR')
+          break
 
   else:
     print('ERROR: Use a .mio file')
--- src/syntax.py
+++ src/syntax.py
@@ -1,5 +1,38 @@
 syn = {
-  'RES': ['PROGRAMA', 'FINPROG', 'SI', 'ENTOCNES', 'SINO', 'FINSI', 'REPITE', 'VECES', 'FINREP', 'IMPRIME', 'LEE']
-  'REL': ['>', '<', '==']
+  'RES': ['PROGRAMA', 'FINPROG', 'SI', 'ENTONCES', 'SINO', 'FINSI', 'REPITE', 'VECES', 'FINREP', 'IMPRIME', 'LEE'],
+  'REL': ['>', '<', '=='],
   'ARI': ['+', '-', '*', '/']
 }
+
+def isRES(word):
+  flag = False
+  for i in syn['RES']:
+    if(word == i):
+      flag = True
+  return flag
+
+def isREL(word):
+  flag = False
+  for i in syn['REL']:
+    if(word == i):
+      flag = True
+  return flag
+
+def isARI(word):
+  flag = False
+  for i in syn['ARI']:
+    if(word == i):
+      flag = True
+  return flag
+
+def isAny(word):
+  flag = isRES(word) or isREL(word) or isARI(word) or (word == '=') or (word[0] == '"' and word[-1] == '"')
+  return flag
+
+def isLine(arr):
+  flag = True
+  for i in arr:
+    # Check if it is any of the reserved words
+    if(not(isAny(i))):
+      flag = False
+  return flag
