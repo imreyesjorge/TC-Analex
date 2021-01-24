73f75492af21bdbf25afa41cdc96fecbbc8f1568
Jorge Reyes <40706868+imreyesjorge@users.noreply.github.com>
2021-01-24 01:47:02 +0000

    feat: anasin started

--- src/anasin/anasin.py
+++ src/anasin/anasin.py
@@ -2,3 +2,8 @@ import sys
 sys.path.append('src/')
 
 from syntax import syn
+
+file = open('{}.lex'.format(sys.argv[1]))
+
+for line in file:
+  print(line, end = '')
--- src/app.py
+++ src/app.py
@@ -3,3 +3,6 @@ import sys
 
 # Run 'analex.py'
 os.system("python src/analex/analex.py {}".format(sys.argv[1]))
+aux = sys.argv[1]
+aux = aux[0:-4]
+os.system("python src/anasin/anasin.py {}".format(aux))
