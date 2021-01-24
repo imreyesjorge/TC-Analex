5a4fb365ab2d7f762bc9911ea99aaa89660c8168
Jorge Reyes <40706868+imreyesjorge@users.noreply.github.com>
2021-01-24 01:28:50 +0000

    feat: write to file

--- src/analex/analex.py
+++ src/analex/analex.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.append('src/')
 
+sys.path.append('src/')
 from syntax import *
 
 # Check correct usage
@@ -12,13 +12,39 @@ if(len(sys.argv) == 2):
     fileLex = open('{}.lex'.format(sys.argv[1].split('.')[0]), 'w+')
     fileSim = open('{}.sim'.format(sys.argv[1].split('.')[0]), 'w+')
 
+    idArr = []
+    txtArr = []
+    valArr = []
+    cID = 1
+    cTXT = 1
+
     for line in file:
       if(line[0] != "#"):
-        if(isLine(line.split())):
-          print(line, end = '')
+        thisArr = toArr(line)     
+        if(isLine(thisArr)):
+          for i in thisArr:
+            if(isDefault(i)):
+              if(isARI(i)):
+                fileLex.write('[op_ar]\n')
+              else:
+                fileLex.write(i + '\n')
+              continue
+            elif(isID(i)):
+              idArr.append([[i],['ID{0:0=2d}'.format(cID)]])
+              fileLex.write('[id] ID' + '{0:0=2d}'.format(cID) + '\n')
+              cID += 1
+            elif(isText(i)):
+              txtArr.append([[i],['TX{0:0=2d}'.format(cTXT)]])
+              fileLex.write('[text] TX' + '{0:0=2d}'.format(cTXT) + '\n')
+              cTXT += 1
+            elif(isNumeric(i)):
+              valArr.append([[i],[octalToDecimal(i)]])
+              fileLex.write('[val]' + '\n')
         else:
           print('ERROR')
           break
+
+    # Write <file>.sim
 
   else:
     print('ERROR: Use a .mio file')
--- src/syntax.py
+++ src/syntax.py
@@ -1,8 +1,41 @@
+import re
+
+file = open('debug.txt', 'w+')
+
 syn = {
   'RES': ['PROGRAMA', 'FINPROG', 'SI', 'ENTONCES', 'SINO', 'FINSI', 'REPITE', 'VECES', 'FINREP', 'IMPRIME', 'LEE'],
-  'REL': ['>', '<', '=='],
+  'REL': ['>', '<', '==', '='],
   'ARI': ['+', '-', '*', '/']
 }
+
+def toArr(str):
+  arr = []
+  currStr = '' 
+  flag = False
+  for i in str:
+    if(i == '\n'):
+      if(currStr != ''):
+        arr.append(currStr)
+    elif(i != ' ' and i != '"'):
+      currStr = currStr + i
+    else:
+      if(flag):
+        if(i != '"'):
+          currStr = currStr + i
+        else:
+          flag = False
+          currStr = currStr + i
+          arr.append(currStr)
+          currStr = ''
+      else:
+        if(i == ' '):
+          if(currStr != ''):
+            arr.append(currStr)
+            currStr = ''
+        else:
+          flag = True
+          currStr = currStr + i
+  return arr
 
 def isRES(word):
   flag = False
@@ -25,14 +58,46 @@ def isARI(word):
       flag = True
   return flag
 
-def isAny(word):
-  flag = isRES(word) or isREL(word) or isARI(word) or (word == '=') or (word[0] == '"' and word[-1] == '"')
-  return flag
-
 def isLine(arr):
   flag = True
+  file.write(' '.join(arr) + '\n')
   for i in arr:
     # Check if it is any of the reserved words
     if(not(isAny(i))):
       flag = False
   return flag
+
+def isDefault(word):
+  flag = isRES(word) or isREL(word) or isARI(word)
+  return flag
+
+def isAny(word):
+  flag = isRES(word) or isREL(word) or isARI(word) or word == '=' or isID(word) or isNumeric(word) or isText(word)
+  return flag
+
+def isID(word):
+  pattern = '[A-Za-z][A-Za-z0-9]+'
+  flag = re.match(pattern, word)  
+  return flag
+
+def isNumeric(word):  
+  pattern = '[0-8]+'
+  flag = re.match(pattern, word) 
+  return flag
+
+def isText(word):
+  pattern = ('"[\sA-Za-z0-9]+"')
+  flag = re.match(pattern, word)
+  return flag
+
+def octalToDecimal(octal):
+  decimal = 0
+  position = 0
+  octal = octal[::-1]
+  for digit in octal:
+    value = int(digit)
+    num = int(8 ** position)
+    equivalence = int(num * value)
+    decimal += equivalence
+    position += 1
+  return decimal
