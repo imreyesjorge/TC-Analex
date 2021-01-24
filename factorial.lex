a2045a932334ee52d5799477480257f093d49cae
Jorge Reyes <40706868+imreyesjorge@users.noreply.github.com>
2021-01-24 01:42:11 +0000

    feat: Analex finished

--- factorial.lex
+++ factorial.lex
@@ -9,25 +9,16 @@ PROGRAMA
 LEE
 [id] ID04
 REPITE
-[id] ID05
 VECES
-[id] ID06
 =
-[id] ID07
 [op_ar]
-[val]
-[id] ID08
 =
-[id] ID09
 [op_ar]
-[id] ID10
 FINREP
 IMPRIME
 [text] TX01
 IMPRIME
-[id] ID11
 IMPRIME
 [text] TX02
 IMPRIME
-[id] ID12
 FINPROG
--- src/analex/analex.py
+++ src/analex/analex.py
@@ -23,6 +23,7 @@ if(len(sys.argv) == 2):
         thisArr = toArr(line)     
         if(isLine(thisArr)):
           for i in thisArr:
+            flag = False
             if(isDefault(i)):
               if(isARI(i)):
                 fileLex.write('[op_ar]\n')
@@ -30,21 +31,44 @@ if(len(sys.argv) == 2):
                 fileLex.write(i + '\n')
               continue
             elif(isID(i)):
-              idArr.append([[i],['ID{0:0=2d}'.format(cID)]])
-              fileLex.write('[id] ID' + '{0:0=2d}'.format(cID) + '\n')
-              cID += 1
+              for this in idArr:
+                if(i == this[0][0]):
+                  flag = True
+              if(flag):
+                flag = False
+              else:
+                idArr.append([[i],['ID{0:0=2d}'.format(cID)]])
+                fileLex.write('[id] ID' + '{0:0=2d}'.format(cID) + '\n')
+                cID += 1
             elif(isText(i)):
               txtArr.append([[i],['TX{0:0=2d}'.format(cTXT)]])
               fileLex.write('[text] TX' + '{0:0=2d}'.format(cTXT) + '\n')
               cTXT += 1
             elif(isNumeric(i)):
-              valArr.append([[i],[octalToDecimal(i)]])
-              fileLex.write('[val]' + '\n')
+              for this in valArr:
+                if(i == this[0][0]):
+                  flag = True
+              if(flag):
+                flag = False
+              else:
+                valArr.append([[i],[octalToDecimal(i)]])
+                fileLex.write('[val]' + '\n')
         else:
           print('ERROR')
           break
 
     # Write <file>.sim
+    fileSim.write('IDS\n')
+    for i in idArr:
+      fileSim.write('{}, {}\n'.format(i[0][0], i[1][0]))
+
+    fileSim.write('\nTXT\n')
+    for i in txtArr:
+      fileSim.write('{}, {}\n'.format(i[0][0], i[1][0]))
+
+    fileSim.write('\nVAL\n')
+    for i in valArr:
+      fileSim.write('{}, {}\n'.format(i[0][0], i[1][0]))
 
   else:
     print('ERROR: Use a .mio file')
